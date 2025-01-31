05714976b2066a69ed97bb13700512f4d600aaf3
Miguel Zaragoza Serrano <mzaragozaserrano@gmail.com>
2025-01-31 08:21:17 +0100

    creating bos

---
+++ domain/src/main/kotlin/com/mzaragozaserrano/domain/bo/ErrorBO.kt
@@ -0,0 +1,10 @@
+package com.mzaragozaserrano.domain.bo
+
+sealed class ErrorBO {
+    data object Connectivity: ErrorBO()
+    data object DataNotFound : ErrorBO()
+    data object DeserializingJSON : ErrorBO()
+    data class Generic(val id: Int) : ErrorBO()
+    data object LoadingData : ErrorBO()
+    data object LoadingURL : ErrorBO()
+}
